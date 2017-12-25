b1560c3629d0fbace4a3719828f9cb604304b433
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-24 19:14:11 -0700

    Create ajax request and edit comments

--- scripts.js
+++ scripts.js
@@ -8,5 +8,8 @@ $(document).ready(function() {
   function getResults() {
     var searchInput = $("#searchInput").val();
     var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchInput + "&callback=?";
-  }
+    $.ajax({
+
+    }); // End AJAX
+  } // End getResults function
 }); // End document ready function
