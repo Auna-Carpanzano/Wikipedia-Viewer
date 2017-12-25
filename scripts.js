aeb0e05e8c8cfec212c386fef50a7a4b2ca95fdd
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-24 19:05:42 -0700

    Add callback function "getResults" to the animation of div with id of "input"

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("#search").click(function() {
     $("#input").animate({
       paddingTop: '0'
-    });
+    }, getResults);
   });
 
 }); // End document ready function
